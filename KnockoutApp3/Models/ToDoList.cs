58a93d8febf59630601c230aae884636f13017f7
jeepmatt <namohtm@gmail.com>
2017-05-03 09:23:18 -0700

    Add project files.

---
+++ KnockoutApp3/Models/ToDoList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockoutApp3.Models
+{
+    public class ToDoList
+    {
+        public List<Tasks> GetTasks()
+        {
+            var ToDo = new List<Tasks>();
+            return ToDo;
+        }
+    }    
+
+    public class Tasks
+    {
+        public string Description { get; set; }
+        public double TimeToComplete { get; set; }
+    }
+}
